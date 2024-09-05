80687db9103ed52ea785e5749c6ed8ba48f85c42
Seda Nur Öz <sedanur3h2001@gmail.com>
2024-09-05 14:11:30 +0300

    .

---
+++ Compare_in_LexicalOrder/EXC1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first, second;
+            Console.Write("Enter the first string:");
+            first = Console.ReadLine();
+            Console.Write("Enter the second string:");
+            second = Console.ReadLine();
+
+            
+            for(int i = 0; i < first.Length; i++)
+            {
+                
+                if (first[i] != second[i])
+                {
+                    if((int)first[i] < (int)second[i])
+                    {
+                        Console.WriteLine("First string comes before second string.");
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Second string comes before first string.");
+                        
+                    }
+                    break;
+                   
+                }
+                
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LongestConsecutiveEqualElements/EXC2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, value = 0 ;
+            Console.Write("Enter the legth of the array:");
+            int length=int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+
+            Console.Write("Enter the integer:");        
+            int num = int.Parse(Console.ReadLine());
+            array[0] = num;
+
+            for (int i = 1; i < length; i++)
+            {
+                Console.Write("Enter the integer:");
+                num = int.Parse(Console.ReadLine());
+                array[i] = num;
+
+                if(array[i]== array[i - 1])
+                {
+                    counter++;
+                    value = num;
+                }
+            }
+
+            Console.Write("Maximal sequence of consecutive equal elements{");
+            while(counter > 0)
+            {
+                Console.Write(value);
+                Console.Write(",");
+                counter--;
+            }
+            Console.Write(value+"}");
+            
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
